628fe6e11f03312f585190b67c02eefa1d17f531
Hayden Hong <hello@haydenhong.com>
2019-03-02 12:33:23 -0800

    Corrected Spotify Login behavior

--- ConvertifyTests/PlaylistTests.swift
+++ ConvertifyTests/PlaylistTests.swift
@@ -17,7 +17,6 @@ class PlaylistTests: XCTestCase {
 
     override func setUp() {
         spotify = SpotifyPlaylistSearcher(token: "")
-
         appleMusic = AppleMusicPlaylistSearcher(token: "")
     }
 
