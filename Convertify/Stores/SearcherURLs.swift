e496ebe023ced8d63e04014b803e4660277090d9
Hayden Hong <hello@haydenhong.com>
2020-02-04 15:21:05 -0800

    Updated regex for apple music, was matching too easily

--- Convertify/Stores/SearcherURLs.swift
+++ Convertify/Stores/SearcherURLs.swift
@@ -9,8 +9,8 @@
 import Foundation
 
 enum SearcherURL {
-    static let spotify = "https://open.spotify.com/(?!playlist).*/\\w{22}\\b((\\?)si=\\w{22}\\b|)"
-    static let appleMusic = "https://(itunes|music).apple.com/\\w{2}\\b/.*/.*/\\w{10}\\b((\\?)i=\\w{10}\\b|)"
+    static let spotify = "https://open.spotify.com/(?!playlist).*/\\w{22}\\b((\\?)si=\\w{22}\\b|$)"
+    static let appleMusic = "https://(itunes|music).apple.com/\\w{2}\\b/.*/.*/\\w{10}\\b((\\?)i=\\w{10}\\b|$)"
 
     // Matches ALL playlist links
     static let playlist = "https://open.spotify.com/playlist/\\w{22}(\\?si=.{22}|)$|https://(itunes|music).apple.com/\\w{2}\\b/playlist/.*/(pl.u-\\w{14}|pl.\\w{32})$"
